e4fa3541053ee0c4ca61deb4033bd893b6d41d31
dingxiaoxv <dingxiaoxv@yahoo.com>
2022-11-03 20:24:36 +0800

    first commit on branch_1

--- main.cpp
+++ main.cpp
@@ -5,5 +5,9 @@ int main()
     std::cout << "Hello GTest" << std::endl;
     std::cout << "GTest is awsome" << std::endl;
     std::cout << "Push Secondly" << std::endl;
+
+    //使用vscode git
+
+    std::cout << "branch_1";
     return 0;
 }
